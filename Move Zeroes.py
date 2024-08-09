b39ab5022a4371c1192ae0b117036379ba41ba57
Dave1humble <149245284+Dave1humble@users.noreply.github.com>
2024-08-09 08:33:59 -0700

    Update Move Zeroes.py

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -3,11 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        for right in range(len(nums)):
-            if nums[right] != 0:
-                if left != right:  
-                    nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-            else:
-                nums[left], nums[right] = nums[right], nums[left] 
+        n = len(nums)
+        l = 0
+        for r in range(n):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
