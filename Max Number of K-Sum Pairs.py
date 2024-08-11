808dbae963251db4bdc85dfd3411450b7c0c813e
Dave1humble <149245284+Dave1humble@users.noreply.github.com>
2024-08-11 05:47:10 -0700

    Update Max Number of K-Sum Pairs.py

--- Max Number of K-Sum Pairs.py
+++ Max Number of K-Sum Pairs.py
@@ -1,15 +1,19 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums_dict = Counter(nums)
-        seen = set()
-        operations = 0
-        for num in nums:
-            if num not in seen and nums_dict[k-num] in nums_dict:
-                if num == k-num:
-                    operations = nums_dict[num]//2
-                else:
-                    operations+=1
-                seen.add(num)
-                seen.add(k-num)
-            
-        return operations
+        nums.sort()  
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        
+        while left < right:
+            current_sum = nums[left] + nums[right]
+            if current_sum == k:
+                count += 1
+                left += 1
+                right -= 1
+            elif current_sum < k:
+                left += 1
+            else:
+                right -= 1
+        
+        return count
