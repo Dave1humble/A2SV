7ec270db9fa41486a9f2d2092b37c4cebbe1b43e
Dave1humble <149245284+Dave1humble@users.noreply.github.com>
2024-08-11 11:07:39 -0700

    Create Circular Array Loop.py

---
+++ Circular Array Loop.py
@@ -0,0 +1,29 @@
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        def next_index(i):
+            return (i + nums[i]) % n
+        for i in range(n):
+            if nums[i] == 0:
+                continue
+            
+            slow, fast = i, next_index(i)
+            while nums[fast] * nums[i] >  0 and nums[next_index(fast)] * nums[i] > 0:
+                if slow == fast:
+                    if slow == next_index(slow):
+                        break
+                    return True
+                slow = next_index(slow)
+                fast = next_index(next_index(fast))
+            slow = i
+            while nums[slow] * nums[i] > 0:
+                next_i  = next_index(slow)
+                nums[slow] = 0
+                slow = next_i
+        return False    
+
+                 
+        
+
+        
